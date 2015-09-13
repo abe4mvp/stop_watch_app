4b98b11b331a5646f735ca51e30e82207dd1d207
Abe Schonfeld <aschonfeld@groupon.com>
2015-09-13 18:20:19 -0400

    Add pause and un pause functionality

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -9,21 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var timer = NSTimer()
-    var count = 0
-    
-    func updateTime() {
-        
-    }
     
     @IBOutlet var time: UILabel!
     
+    @IBOutlet var startButton: UIButton!
+
+    var timer = NSTimer()
+    var count = 0
+    var isPaused = true
+    
+    func updateTime() {
+        count++
+        time.text = String(count)
+    }
+    
+    func updateStartButton() {
+        startButton.setTitle(isPaused ? "Start" : "Pause", forState: .Normal)
+    }
+    
+
+    
     @IBAction func start(sender: AnyObject) {
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+        
+        if isPaused {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+            isPaused = false
+            updateStartButton()
+        } else {
+            timer.invalidate()
+            isPaused = true
+            updateStartButton()
+        }
+        
         
     }
     @IBAction func reset(sender: AnyObject) {
+        timer.invalidate()
+        isPaused = true
+        updateStartButton()
+        count = 0
+        time.text = "0"
+        
     }
     
     override func viewDidLoad() {
