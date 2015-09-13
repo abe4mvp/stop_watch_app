f4b54f0edc7dcdbba0cce787a5bef73c15614f5e
Abe Schonfeld <aschonfeld@groupon.com>
2015-09-13 16:26:26 -0400

    Basic view and outlets

--- Stop Watch/ViewController.swift
+++ Stop Watch/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var count = 0
+    
+    func updateTime() {
+        
+    }
+    
+    @IBOutlet var time: UILabel!
+    
+    @IBAction func start(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+        
+    }
+    @IBAction func reset(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
